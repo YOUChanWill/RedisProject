27ff4718b1b8b1a2e4daf409fc1be24ff6bff6b0
CHAN W1LL <303661031@qq.com>
2023-07-02 12:09:37 +0800

    使用Redis实现分布式锁

--- Rush2Purchase/src/main/java/you/chanwill/lock/SimpleRedisLock.java
+++ Rush2Purchase/src/main/java/you/chanwill/lock/SimpleRedisLock.java
@@ -2,18 +2,34 @@ package you.chanwill.lock;
 
 import org.springframework.data.redis.core.StringRedisTemplate;
 
+import java.util.concurrent.TimeUnit;
+
 public class SimpleRedisLock implements ILock{
 
     private String name;
     private StringRedisTemplate stringRedisTemplate;
 
+    public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
+        this.name = name;
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    private static final String KEY_PREFIX = "lock:";
+
     @Override
     public boolean tryLock(long timeoustSec) {
-        return false;
+
+        // 获取线程标识
+        long threadId = Thread.currentThread().getId();
+        // 获取锁
+        Boolean success = stringRedisTemplate.opsForValue()
+                .setIfAbsent(KEY_PREFIX + name, threadId + "", timeoustSec, TimeUnit.SECONDS);
+
+        return Boolean.TRUE.equals(success);
     }
 
     @Override
     public void unlock() {
-
+        stringRedisTemplate.delete(KEY_PREFIX + name);
     }
 }
