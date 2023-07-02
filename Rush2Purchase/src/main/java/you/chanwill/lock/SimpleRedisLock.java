25c416a416eb3f9303bf0b64c15cfb6ab8891103
CHAN W1LL <303661031@qq.com>
2023-07-02 12:36:08 +0800

    优化基于Redis的分布式锁

--- Rush2Purchase/src/main/java/you/chanwill/lock/SimpleRedisLock.java
+++ Rush2Purchase/src/main/java/you/chanwill/lock/SimpleRedisLock.java
@@ -2,6 +2,7 @@ package you.chanwill.lock;
 
 import org.springframework.data.redis.core.StringRedisTemplate;
 
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
@@ -16,11 +17,14 @@ public class SimpleRedisLock implements ILock{
 
     private static final String KEY_PREFIX = "lock:";
 
+    // 线程标识
+    private static final  String  ID_PREFIX = UUID.randomUUID().toString() + "-";
+
     @Override
     public boolean tryLock(long timeoustSec) {
 
         // 获取线程标识
-        long threadId = Thread.currentThread().getId();
+        String  threadId = ID_PREFIX + Thread.currentThread().getId();
         // 获取锁
         Boolean success = stringRedisTemplate.opsForValue()
                 .setIfAbsent(KEY_PREFIX + name, threadId + "", timeoustSec, TimeUnit.SECONDS);
@@ -30,6 +34,14 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public void unlock() {
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+        // 获取线程标识
+        String  threadId = ID_PREFIX + Thread.currentThread().getId();
+
+        // 获取锁中的标识
+        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+
+        if (threadId.equals(id)){
+            stringRedisTemplate.delete(KEY_PREFIX + name);
+        }
     }
 }
