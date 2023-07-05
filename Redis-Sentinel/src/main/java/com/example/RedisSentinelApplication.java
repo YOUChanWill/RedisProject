a3349a11e628fb2407a1b80494322aeafe807792
CHAN W1LL <303661031@qq.com>
2023-07-05 12:25:35 +0800

    配置Redis哨兵

---
+++ Redis-Sentinel/src/main/java/com/example/RedisSentinelApplication.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import io.lettuce.core.ReadFrom;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+
+@SpringBootApplication
+public class RedisSentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisSentinelApplication.class, args);
+    }
+
+    @Bean
+    public LettuceClientConfigurationBuilderCustomizer clientConfigurationBuilderCustomizer(){
+        return new LettuceClientConfigurationBuilderCustomizer() {
+            @Override
+            public void customize(LettuceClientConfiguration.LettuceClientConfigurationBuilder clientConfigurationBuilder) {
+                clientConfigurationBuilder.readFrom(ReadFrom.REPLICA_PREFERRED);
+            }
+        };
+    }
+
+}
